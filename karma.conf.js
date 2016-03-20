27cf93032c4d6e873df33bc900361d424279eec4
Lukasz Wolnik <lukasz@wolnik.co.uk>
2016-03-20 13:21:40 +0000

    Unit testing

---
+++ karma.conf.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: [
+      './test/main.js'
+    ],
+    preprocessors: {
+      './test/main.js': ['webpack', 'sourcemap']
+    },
+    reporters: ['dots'],
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+			  test: /\.jsx?$/,
+			  exclude: /node_modules/,
+			  loader: 'babel',
+			  query: {
+				  presets: ['react', 'es2015']
+			  }
+		  }
+        ]
+      }
+    },
+	webpackServer: {
+      noInfo: true
+    }
+  });
+};
---
+++ test/test.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import expect from 'expect';
+
+describe('root', function () {
+	it('sanity check', function () {
+		expect(true).toBe(true);
+	});
+});
